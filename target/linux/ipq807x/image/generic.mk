4838ff1988373e816584f16fc3167befdf7f2d3f
small-white-rabbit <525307811@qq.com>
2022-07-27 00:11:51 +0800

    默认ax3600

--- target/linux/ipq807x/image/generic.mk
+++ target/linux/ipq807x/image/generic.mk
@@ -17,6 +17,29 @@ define Device/UbiFit
 	IMAGE/nand-sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
 
+define Device/xiaomi_ax3600
+	$(call Device/FitImage)
+	$(call Device/UbiFit)
+	DEVICE_VENDOR := Xiaomi
+	DEVICE_MODEL := AX3600
+	BLOCKSIZE := 128k
+	PAGESIZE := 2048
+	DEVICE_DTS_CONFIG := config@ac04
+	SOC := ipq8071
+	DEVICE_PACKAGES := ath10k-firmware-qca9887-ct ipq-wifi-xiaomi_ax3600 \
+	kmod-ath10k-ct uboot-envtools
+endef
+TARGET_DEVICES += xiaomi_ax3600
+
+
+define Device/redmi_ax6
+	$(call Device/xiaomi_ax3600)
+	DEVICE_VENDOR := Redmi
+	DEVICE_MODEL := AX6
+	DEVICE_PACKAGES := ipq-wifi-redmi_ax6 uboot-envtools
+endef
+TARGET_DEVICES += redmi_ax6
+
 define Device/qnap_301w
 	$(call Device/FitImage)
 	DEVICE_VENDOR := QNAP
@@ -31,28 +54,6 @@ define Device/qnap_301w
 	DEVICE_PACKAGES := ipq-wifi-qnap_301w e2fsprogs kmod-fs-ext4 losetup
 endef
 TARGET_DEVICES += qnap_301w
-
-define Device/redmi_ax6
-	$(call Device/xiaomi_ax3600)
-	DEVICE_VENDOR := Redmi
-	DEVICE_MODEL := AX6
-	DEVICE_PACKAGES := ipq-wifi-redmi_ax6 uboot-envtools
-endef
-TARGET_DEVICES += redmi_ax6
-
-define Device/xiaomi_ax3600
-	$(call Device/FitImage)
-	$(call Device/UbiFit)
-	DEVICE_VENDOR := Xiaomi
-	DEVICE_MODEL := AX3600
-	BLOCKSIZE := 128k
-	PAGESIZE := 2048
-	DEVICE_DTS_CONFIG := config@ac04
-	SOC := ipq8071
-	DEVICE_PACKAGES := ath10k-firmware-qca9887-ct ipq-wifi-xiaomi_ax3600 \
-	kmod-ath10k-ct uboot-envtools
-endef
-TARGET_DEVICES += xiaomi_ax3600
 
 define Device/zte_mf269
 	$(call Device/FitImage)
